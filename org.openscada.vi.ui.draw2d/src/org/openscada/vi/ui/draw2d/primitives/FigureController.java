c237633967790aa01de77c0a79689de7ec207a92
Jens Reimann <jens.reimann@th4-systems.com>
2011-08-18 14:49:33 +0200

    implementing blinking colors

--- org.openscada.vi.ui.draw2d/src/org/openscada/vi/ui/draw2d/primitives/FigureController.java
+++ org.openscada.vi.ui.draw2d/src/org/openscada/vi/ui/draw2d/primitives/FigureController.java
@@ -1,19 +1,23 @@
 package org.openscada.vi.ui.draw2d.primitives;
 
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.StringTokenizer;
 
 import javax.script.ScriptException;
 
+import org.eclipse.draw2d.AncestorListener;
+import org.eclipse.draw2d.IFigure;
 import org.eclipse.draw2d.MouseEvent;
 import org.eclipse.draw2d.MouseListener;
 import org.eclipse.draw2d.geometry.PrecisionDimension;
-import org.eclipse.jface.resource.ColorDescriptor;
 import org.eclipse.jface.resource.ResourceManager;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.widgets.Display;
+import org.openscada.ui.utils.blink.AbstractBlinker;
 import org.openscada.utils.script.ScriptExecutor;
 import org.openscada.vi.model.VisualInterface.Cursor;
 import org.openscada.vi.model.VisualInterface.Dimension;
@@ -24,6 +28,30 @@ import org.openscada.vi.ui.draw2d.SymbolController;
 
 public abstract class FigureController implements Controller
 {
+
+    private static interface ColorApplier
+    {
+        public void applyColor ( IFigure figure, Color color );
+    }
+
+    private static class ForegroundApplier implements ColorApplier
+    {
+        @Override
+        public void applyColor ( final IFigure figure, final Color color )
+        {
+            figure.setForegroundColor ( color );
+        }
+    }
+
+    private static class BackgroundApplier implements ColorApplier
+    {
+        @Override
+        public void applyColor ( final IFigure figure, final Color color )
+        {
+            figure.setBackgroundColor ( color );
+        }
+    }
+
     private final ResourceManager manager;
 
     private final SymbolController controller;
@@ -32,14 +60,44 @@ public abstract class FigureController implements Controller
 
     private ScriptExecutor onDoubleClick;
 
+    private final Map<Object, BaseColor> colorHandler = new HashMap<Object, FigureController.BaseColor> ();
+
     public FigureController ( final SymbolController controller, final ResourceManager manager )
     {
         this.manager = manager;
         this.controller = controller;
     }
 
+    protected void internalDispose ()
+    {
+        for ( final BaseColor color : this.colorHandler.values () )
+        {
+            color.dispose ();
+        }
+        this.colorHandler.clear ();
+    }
+
     protected void applyCommon ( final Figure figure )
     {
+        getFigure ().addAncestorListener ( new AncestorListener () {
+            @Override
+            public void ancestorAdded ( final IFigure ancestor )
+            {
+                System.out.println ( "added: " + ancestor );
+            }
+
+            @Override
+            public void ancestorMoved ( final IFigure ancestor )
+            {
+            }
+
+            @Override
+            public void ancestorRemoved ( final IFigure ancestor )
+            {
+                System.out.println ( "removed: " + ancestor );
+            };
+        } );
+
         setBackgroundColor ( figure.getBackgroundColor () );
         setForegroundColor ( figure.getForegroundColor () );
 
@@ -153,14 +211,120 @@ public abstract class FigureController implements Controller
         getFigure ().setPreferredSize ( dim );
     }
 
+    private static interface BaseColor
+    {
+        public void start ();
+
+        public void dispose ();
+    }
+
+    private static class DefaultColor implements BaseColor
+    {
+        private final IFigure figure;
+
+        private final ColorApplier applier;
+
+        public DefaultColor ( final IFigure figure, final ColorApplier applier )
+        {
+            this.figure = figure;
+            this.applier = applier;
+        }
+
+        @Override
+        public void start ()
+        {
+            this.applier.applyColor ( this.figure, null );
+        }
+
+        @Override
+        public void dispose ()
+        {
+            // nothing to do  
+        }
+    }
+
+    private static class StaticColor implements BaseColor
+    {
+        private final IFigure figure;
+
+        private final ColorApplier applier;
+
+        private final Color color;
+
+        public StaticColor ( final IFigure figure, final ColorApplier applier, final Color color )
+        {
+            this.figure = figure;
+            this.applier = applier;
+            this.color = color;
+        }
+
+        @Override
+        public void start ()
+        {
+            this.applier.applyColor ( this.figure, this.color );
+        }
+
+        @Override
+        public void dispose ()
+        {
+            // nothing to do
+        }
+    }
+
+    private static class BlinkingColor extends AbstractBlinker implements BaseColor
+    {
+        private final IFigure figure;
+
+        private final ColorApplier applier;
+
+        private final Color onColor;
+
+        private final Color offColor;
+
+        private final int frequency;
+
+        public BlinkingColor ( final IFigure figure, final ColorApplier applier, final Color onColor, final Color offColor, final int frequency )
+        {
+            this.figure = figure;
+            this.applier = applier;
+            this.onColor = onColor;
+            this.offColor = offColor;
+            this.frequency = frequency;
+        }
+
+        @Override
+        public void start ()
+        {
+            enableBlinking ( this.frequency );
+        }
+
+        @Override
+        public void toggle ( final boolean on )
+        {
+            this.applier.applyColor ( this.figure, on ? this.onColor : this.offColor );
+        }
+    }
+
     public void setBackgroundColor ( final String color )
     {
-        getFigure ().setBackgroundColor ( makeColor ( color ) );
+        final BaseColor colorHandler = makeColorHandler ( color, new BackgroundApplier () );
+        setColor ( "backgroundColor", colorHandler );
     }
 
     public void setForegroundColor ( final String color )
     {
-        getFigure ().setForegroundColor ( makeColor ( color ) );
+        final BaseColor colorHandler = makeColorHandler ( color, new ForegroundApplier () );
+        setColor ( "foregroundColor", colorHandler );
+    }
+
+    protected void setColor ( final Object key, final BaseColor colorHandler )
+    {
+        final BaseColor oldColorHandler = this.colorHandler.put ( key, colorHandler );
+        if ( oldColorHandler != null )
+        {
+            oldColorHandler.dispose ();
+        }
+        colorHandler.start ();
     }
 
     public void setVisible ( final boolean flag )
@@ -177,35 +341,67 @@ public abstract class FigureController implements Controller
         return new PrecisionDimension ( dimension.getWidth (), dimension.getHeight () );
     }
 
-    protected Color makeColor ( final String color )
+    protected BaseColor makeColorHandler ( final String color, final ColorApplier applier )
     {
         if ( color == null )
         {
-            return null;
+            return new DefaultColor ( getFigure (), applier );
         }
 
+        if ( color.startsWith ( "#" ) && !color.contains ( "|" ) )
+        {
+            return new StaticColor ( getFigure (), applier, createColor ( makeColor ( color ) ) );
+        }
+        else if ( color.startsWith ( "#" ) && color.contains ( "|" ) )
+        {
+            final StringTokenizer tok = new StringTokenizer ( color, "|" );
+            final Color onColor = createColor ( makeColor ( tok.nextToken () ) );
+            Color offColor;
+            int frequency = 1;
+            if ( tok.hasMoreElements () )
+            {
+                offColor = createColor ( makeColor ( tok.nextToken () ) );
+            }
+            else
+            {
+                offColor = null;
+            }
+            if ( tok.hasMoreElements () )
+            {
+                frequency = Integer.parseInt ( tok.nextToken () );
+            }
+            return new BlinkingColor ( getFigure (), applier, onColor, offColor, frequency );
+        }
+        return new DefaultColor ( getFigure (), applier );
+    }
+
+    protected Color createColor ( final RGB rgb )
+    {
+        if ( rgb == null )
+        {
+            return null;
+        }
+        return this.manager.createColor ( rgb );
+    }
+
+    protected static RGB makeColor ( final String color )
+    {
         if ( color.startsWith ( "#" ) )
         {
             if ( color.length () == 1 + 3 )
             {
-                return this.manager.createColor ( makeColor ( color.substring ( 1, 2 ), color.substring ( 2, 3 ), color.substring ( 3, 4 ) ) );
+                return makeRGB ( color.substring ( 1, 2 ), color.substring ( 2, 3 ), color.substring ( 3, 4 ) );
             }
             else if ( color.length () >= 1 + 6 )
             {
-                return this.manager.createColor ( makeColor ( color.substring ( 1, 3 ), color.substring ( 3, 5 ), color.substring ( 5, 7 ) ) );
+                return makeRGB ( color.substring ( 1, 3 ), color.substring ( 3, 5 ), color.substring ( 5, 7 ) );
             }
-            return null;
         }
         return null;
     }
 
-    protected static ColorDescriptor makeColor ( final String r, final String g, final String b )
+    protected static RGB makeRGB ( final String r, final String g, final String b )
     {
-        return makeColor ( Integer.parseInt ( r, 16 ), Integer.parseInt ( g, 16 ), Integer.parseInt ( b, 16 ) );
-    }
-
-    protected static ColorDescriptor makeColor ( final int r, final int g, final int b )
-    {
-        return ColorDescriptor.createFrom ( new RGB ( r, g, b ) );
+        return new RGB ( Integer.parseInt ( r, 16 ), Integer.parseInt ( g, 16 ), Integer.parseInt ( b, 16 ) );
     }
 }
