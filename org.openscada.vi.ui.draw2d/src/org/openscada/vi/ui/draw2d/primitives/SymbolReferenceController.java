c65601cb13cbffb26c5cf523d0cd0b29468a195a
Jens Reimann <jens.reimann@th4-systems.com>
2011-08-19 09:28:43 +0200

    log to logger

--- org.openscada.vi.ui.draw2d/src/org/openscada/vi/ui/draw2d/primitives/SymbolReferenceController.java
+++ org.openscada.vi.ui.draw2d/src/org/openscada/vi/ui/draw2d/primitives/SymbolReferenceController.java
@@ -20,9 +20,14 @@ import org.openscada.vi.ui.draw2d.Helper;
 import org.openscada.vi.ui.draw2d.SymbolController;
 import org.openscada.vi.ui.draw2d.SymbolLoader;
 import org.openscada.vi.ui.draw2d.ViewElementFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SymbolReferenceController implements Controller
 {
+
+    private final static Logger logger = LoggerFactory.getLogger ( SymbolReferenceController.class );
+
     private final LayeredPane figure;
 
     public SymbolReferenceController ( final SymbolController controller, final SymbolReference symbolReference, final ViewElementFactory factory, final ResourceManager manager )
@@ -44,7 +49,6 @@ public class SymbolReferenceController implements Controller
 
         try
         {
-
             final URI uri = URI.createURI ( symbolReference.getUri () );
             final SymbolLoader symbolLoader = factory.load ( uri );
 
@@ -69,6 +73,7 @@ public class SymbolReferenceController implements Controller
         }
         catch ( final Exception e )
         {
+            logger.warn ( "Failed to create symbol", e );
             StatusManager.getManager ().handle ( StatusHelper.convertStatus ( Activator.PLUGIN_ID, e ), StatusManager.LOG );
             layer.add ( Helper.createErrorFigure ( e ) );
         }
