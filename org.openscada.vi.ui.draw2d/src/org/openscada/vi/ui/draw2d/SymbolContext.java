87d4040a48f246943e11dfa761a56260b62794ae
Jens Reimann <jens.reimann@th4-systems.com>
2011-08-19 16:10:41 +0200

    add command support

--- org.openscada.vi.ui.draw2d/src/org/openscada/vi/ui/draw2d/SymbolContext.java
+++ org.openscada.vi.ui.draw2d/src/org/openscada/vi/ui/draw2d/SymbolContext.java
@@ -1,6 +1,16 @@
 package org.openscada.vi.ui.draw2d;
 
+import java.util.Map;
 import java.util.Properties;
+
+import org.eclipse.core.commands.Command;
+import org.eclipse.core.commands.Parameterization;
+import org.eclipse.core.commands.ParameterizedCommand;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.commands.ICommandService;
+import org.eclipse.ui.handlers.IHandlerService;
+import org.eclipse.ui.statushandlers.StatusManager;
+import org.openscada.ui.utils.status.StatusHelper;
 
 public class SymbolContext
 {
@@ -36,4 +46,31 @@ public class SymbolContext
         this.controller.unregisterItem ( name );
     }
 
+    public void executeCommand ( final String commandId, final Map<String, String> eventData )
+    {
+
+        try
+        {
+            final ICommandService commandService = (ICommandService)PlatformUI.getWorkbench ().getService ( ICommandService.class );
+            final IHandlerService handlerService = (IHandlerService)PlatformUI.getWorkbench ().getService ( IHandlerService.class );
+
+            final Command command = commandService.getCommand ( commandId );
+
+            final Parameterization[] parameterizations = new Parameterization[eventData.size ()];
+
+            int i = 0;
+            for ( final Map.Entry<String, String> entry : eventData.entrySet () )
+            {
+                parameterizations[i] = new Parameterization ( command.getParameter ( entry.getKey () ), entry.getValue () );
+                i++;
+            }
+            final ParameterizedCommand parameterCommand = new ParameterizedCommand ( command, parameterizations );
+
+            handlerService.executeCommand ( parameterCommand, null );
+        }
+        catch ( final Exception e )
+        {
+            StatusManager.getManager ().handle ( StatusHelper.convertStatus ( Activator.PLUGIN_ID, e ), StatusManager.BLOCK );
+        }
+    }
 }
