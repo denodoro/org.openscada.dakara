4baf53cbe614484b4e4dafa602437ac40e692ca3
Jens Reimann <jens.reimann@th4-systems.com>
2012-09-05 14:44:03 +0200

    add warning

--- org.openscada.vi.ui.draw2d/src/org/openscada/vi/ui/draw2d/SummaryInformation.java
+++ org.openscada.vi.ui.draw2d/src/org/openscada/vi/ui/draw2d/SummaryInformation.java
@@ -1,6 +1,6 @@
 /*
  * This file is part of the openSCADA project
- * Copyright (C) 2006-2011 TH4 SYSTEMS GmbH (http://th4-systems.com)
+ * Copyright (C) 2006-2012 TH4 SYSTEMS GmbH (http://th4-systems.com)
  *
  * openSCADA is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License version 3
@@ -105,6 +105,11 @@ public class SummaryInformation
     public boolean isAlarm ()
     {
         return isAttribute ( "alarm", false );
+    }
+
+    public boolean isWarning ()
+    {
+        return isAttribute ( "warning", false );
     }
 
     public boolean isManual ()
