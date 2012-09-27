a7d6f11a49e4841731c7b6b7d7ea82b2b2da4d6b
Jens Reimann <jens.reimann@th4-systems.com>
2012-09-27 11:55:41 +0200

    open with a "wait shell"

--- org.openscada.vi.details/src/org/openscada/vi/details/handler/ShowDetailDialog.java
+++ org.openscada.vi.details/src/org/openscada/vi/details/handler/ShowDetailDialog.java
@@ -23,11 +23,17 @@ import java.util.Map;
 
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
 import org.openscada.ui.databinding.AbstractSelectionHandler;
 import org.openscada.vi.details.DetailViewDialog;
 
 public class ShowDetailDialog extends AbstractSelectionHandler
 {
+
+    private final boolean useWaitShell = !Boolean.getBoolean ( "org.openscada.vi.details.handler.disableWaitShell" );
 
     @SuppressWarnings ( "unchecked" )
     @Override
@@ -36,10 +42,49 @@ public class ShowDetailDialog extends AbstractSelectionHandler
         final String detailViewId = event.getParameter ( "org.openscada.vi.details.showDetailDialog.id" );
         final Map<String, String> parameters = (Map<String, String>)event.getObjectParameterForExecution ( "org.openscada.vi.details.showDetailDialog.parameters" );
 
-        final DetailViewDialog dlg = new DetailViewDialog ( getShell (), detailViewId, parameters );
-        dlg.open ();
+        if ( this.useWaitShell )
+        {
+            openWithWaitShell ( getShell (), detailViewId, parameters );
+        }
+        else
+        {
+            open ( getShell (), detailViewId, parameters );
+        }
 
         return null;
+    }
+
+    protected void openWithWaitShell ( final Shell parentShell, final String detailViewId, final Map<String, String> parameters )
+    {
+
+        final Shell waitShell = new Shell ( parentShell, SWT.PRIMARY_MODAL | SWT.BORDER );
+        waitShell.setLayout ( new FillLayout () );
+        final Label label = new Label ( waitShell, SWT.NONE );
+        label.setText ( "Opening view…" );
+
+        waitShell.pack ();
+        waitShell.open ();
+
+        // make sure the text is visible
+        waitShell.getDisplay ().update ();
+
+        try
+        {
+            open ( parentShell, detailViewId, parameters );
+        }
+        finally
+        {
+            // close the wait shell
+            waitShell.close ();
+        }
+
+    }
+
+    protected void open ( final Shell parentShell, final String detailViewId, final Map<String, String> parameters )
+    {
+        // now open the real dialog
+        final DetailViewDialog dlg = new DetailViewDialog ( parentShell, detailViewId, parameters );
+        dlg.open ();
     }
 
 }
