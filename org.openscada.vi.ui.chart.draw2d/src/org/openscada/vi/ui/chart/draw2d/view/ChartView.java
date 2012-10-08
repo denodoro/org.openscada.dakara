0147dfb9285aaa7e4526b58a174bab3d036d3d98
Jens Reimann <jens.reimann@th4-systems.com>
2012-10-08 10:21:44 +0200

    cleanup

--- org.openscada.vi.ui.chart.draw2d/src/org/openscada/vi/ui/chart/draw2d/view/ChartView.java
+++ org.openscada.vi.ui.chart.draw2d/src/org/openscada/vi/ui/chart/draw2d/view/ChartView.java
@@ -1,3 +1,22 @@
+/*
+ * This file is part of the openSCADA project
+ * Copyright (C) 2006-2012 TH4 SYSTEMS GmbH (http://th4-systems.com)
+ *
+ * openSCADA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * openSCADA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with openSCADA. If not, see
+ * <http://opensource.org/licenses/lgpl-3.0.html> for a copy of the LGPLv3 License.
+ */
+
 package org.openscada.vi.ui.chart.draw2d.view;
 
 import org.eclipse.jface.resource.ResourceManager;
@@ -20,7 +39,6 @@ import org.openscada.vi.ui.user.ViewManagerContext;
 
 public class ChartView extends AbstractViewInstance
 {
-
     private ChartViewer chart;
 
     private final Composite viewHolder;
@@ -40,7 +58,6 @@ public class ChartView extends AbstractViewInstance
         super ( viewManagerContext, viewManager, manager, descriptor, toolbar, evaluationService );
         this.showSelector = showSelector;
         this.viewHolder = viewHolder;
-
     }
 
     @Override
